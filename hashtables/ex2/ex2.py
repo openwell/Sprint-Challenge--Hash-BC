10e1f8764e04e56e6cf7f77c29773e35367b34cf
Lambda <lambda@Lambdas-MacBook-Pro-3.local>
2019-11-01 14:09:02 +0100

    ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -13,11 +13,22 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    hashtable = HashTable(length)
+    ht = HashTable(length)
     route = [None] * length
 
     """
     YOUR CODE HERE
     """
+    for i in range(length): # 1
+        hash_table_insert(ht, tickets[i].source, tickets[i].destination )
 
-    return route
+    first_item = hash_table_retrieve(ht, "NONE") # 2
+    route[0] = first_item # 3
+
+    for i in range(1, length): # 4
+        next_item = hash_table_retrieve(ht, route[i-1]) # 5
+        # if next_item != 'NONE':
+        route[i] = next_item # 6
+        print("Updated Route: ", route)
+
+    return route # 7
